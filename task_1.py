b78f6cc5b2125da5f9f7af92406ef2a51dfd6002
anastasiia-natina <132943209+anastasiia-natina@users.noreply.github.com>
2024-05-10 22:19:10 +0200

    init

--- task_1.py
+++ task_1.py
@@ -2,22 +2,30 @@ import sys
 import os
 import shutil
 
+
 def sort_files(source_dir, dest_dir):
+    
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
-    for root,dest_dir, files in os.walk(source_dir):
-        for file in files:
-            full_path = os.path.join(root, file)
-            ext = os.path.splitext(file)[1]
-            ext_dir = os.path.join(dest_dir, ext[1:])
+    for entry in os.listdir(source_dir):
+        full_path = os.path.join(source_dir, entry)
+
+        if os.path.isdir(full_path):
+
+            sort_files(full_path, dest_dir)
+        else:
+            ext = os.path.splitext(entry)[1] 
+            ext_dir = os.path.join(dest_dir, ext)  
 
             if not os.path.exists(ext_dir):
                 os.makedirs(ext_dir)
 
             shutil.copy(full_path, ext_dir)
 
+
 if __name__ == "__main__":
+
     if len(sys.argv) < 2:
         print("Необхідно вказати шлях до вихідної директорії.")
         sys.exit(1)
